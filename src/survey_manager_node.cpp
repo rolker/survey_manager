bafe92c44aa2b29114dec23fa8c2299bd0c76cb8
Roland Arsenault <roland@ccom.unh.edu>
2019-05-07 23:22:37 -0400

    removed logging

--- src/survey_manager_node.cpp
+++ src/survey_manager_node.cpp
@@ -1,13 +1,11 @@
 #include "ros/ros.h"
 #include "survey_manager/SonarCoverage.h"
 #include "kongsberg_em_control/EMControl.h"
-#include "project11/mutex_protected_bag_writer.h"
+//#include "project11/mutex_protected_bag_writer.h"
 #include "std_msgs/Int32.h"
 #include <regex>
 #include "boost/date_time/posix_time/posix_time.hpp"
 #include "nodelet/loader.h"
-
-MutexProtectedBagWriter log_bag;
 
 void wptIndexCallback(const std_msgs::Int32::ConstPtr &message)
 {
@@ -25,7 +23,6 @@ int main(int argc, char **argv)
     boost::posix_time::ptime now = ros::WallTime::now().toBoost();
     std::string iso_now = std::regex_replace(boost::posix_time::to_iso_extended_string(now),std::regex(":"),"-");
     std::string log_filename = "nodes/survey_manager-"+iso_now+".bag";
-    log_bag.open(log_filename, rosbag::bagmode::Write);
     
     ros::Subscriber wptIndexSub = n.subscribe("/moos/wpt_index",10,&wptIndexCallback);
     
